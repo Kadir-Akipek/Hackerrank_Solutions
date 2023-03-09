c30683560d50306205cc10afd5faec67c792add9
Kadir-Akipek <110154102+Kadir-Akipek@users.noreply.github.com>
2023-03-09 21:05:23 +0300

    Solution

---
+++ Append_andDelete.py
@@ -0,0 +1,31 @@
+def appendAndDelete(s, t, k):
+    cs = 0
+    ct = 0
+    
+    if len(s) < len(t):
+        for i in range(len(s)):
+            if s[i] == t[i]:
+                cs += 1
+                ct += 1
+            else:
+                break
+    
+    else:
+        for i in range(len(t)):
+            if s[i] == t[i]:
+                cs += 1
+                ct += 1
+            else:
+                break
+                
+    mneed = (len(s) - cs) + (len(t) - ct)
+    
+
+    if k > len(s) + len(t):
+        return "Yes"
+    
+    if k >= mneed and  (k - mneed) % 2 == 0 :
+        return "Yes"
+    
+    else:
+        return "No"
