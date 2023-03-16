c9e5d25872c80163365739f92288af2ad42c5fe2
Kadir-Akipek <110154102+Kadir-Akipek@users.noreply.github.com>
2023-03-16 16:05:57 +0300

    Update Library_Fine.py

--- Library_Fine.py
+++ Library_Fine.py
@@ -1,18 +1,18 @@
 def libraryFine(d1, m1, y1, d2, m2, y2):
     if m1 == m2 and y1 == y2:
-        if d1-d2 <= 0:
+        if d1<=d2:
             return 0
         else:
             return (d1-d2) * 15
 
     elif m1 != m2 and y1 == y2:
-        if m1-m2 <= 0:
+        if m1<= m2:
             return 0
         else:
             return (m1-m2) * 500
         
     if y1 != y2:
-        if y1-y2 <= 0:
+        if y1<=y2:
             return 0
         else:
             return (y1-y2) * 10000
