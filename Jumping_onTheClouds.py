b92394d541fe5468278f23600d19a6f5eeb4026a
Kadir-Akipek <110154102+Kadir-Akipek@users.noreply.github.com>
2023-03-19 17:51:42 +0300

    Solution

---
+++ Jumping_onTheClouds.py
@@ -0,0 +1,22 @@
+def jumpingOnClouds(c):
+    jump = 0
+    i = 0
+    
+    while(len(c) - i > 2):
+        if c[i+1] == 1:
+            i += 2
+            jump += 1
+            
+        elif c[i+2] == 1:
+            i += 1
+            jump += 1
+            
+        else:
+            i += 2
+            jump += 1
+            
+    if i == len(c) - 1:
+        return jump
+    else:
+        jump += 1
+        return 
