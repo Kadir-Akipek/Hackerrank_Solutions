644f2a92d044005fa2cf53aaa0b7fb81ad15f33f
Kadir-Akipek <110154102+Kadir-Akipek@users.noreply.github.com>
2023-04-06 12:44:29 +0300

    Solution

---
+++ Modified_Kaprekar.py
@@ -0,0 +1,16 @@
+def kaprekarNumbers(p, q):
+    res = []
+
+    for i in range(p, q+1):
+        sqr = str(i ** 2)
+        n = len(sqr)
+
+        if i == 1:
+            res.append(i)
+        elif n > 1 and i == int(sqr[:n//2]) + int(sqr[n//2:]):
+            res.append(i)
+
+    if len(res) == 0:
+        print("INVALID RANGE")
+    else:
+        print(*res)
